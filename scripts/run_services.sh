e277f04547bdb1c6645fbbbf3c5358245bf227eb
SalaheddineLK <135765994+cornerggs@users.noreply.github.com>
2025-05-25 17:15:57 -0400

    Update run_services.sh
    cc

--- scripts/run_services.sh
+++ scripts/run_services.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-
 echo "demarrage des conteneur Docker"
 cd "$(dirname "$0")"
 
-docker-compose up -d
+sudo docker-compose up -d
 sleep 5
 
 echo "lancement du service PostgreSQL"
 cd ..
 cd postgresql
-./run_postgresql.sh
+sudo ./run_postgresql.sh
 cd ..
 
 echo "lancement du srv-translate..."
 cd srv-translate
-./mvnw clean package -DskipTests
+sudo ./mvnw clean package -DskipTests
 cd target
-nohup java -jar srvtranslate-0.0.1-SNAPSHOT.jar > srv-translate.log 2>&1 &
+sudo nohup java -jar srvtranslate-0.0.1-SNAPSHOT.jar > srv-translate.log 2>&1 &
 cd ../..
 
 echo "Compilation et lancement de client-cons-db..."
 cd cons-db-srv
-./mvnw clean package -DskipTests
+sudo ./mvnw clean package -DskipTests
 cd target
-nohup java -jar clientConsDb-0.0.1-SNAPSHOT.jar > client-cons-db.log 2>&1 &
+sudo nohup java -jar clientConsDb-0.0.1-SNAPSHOT.jar > client-cons-db.log 2>&1 &
 cd ../..
 
 echo "les services sont lancés !lancer les clients CLI"
