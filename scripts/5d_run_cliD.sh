139748454fee8de763974571ebe9c398b70db478
soufiane <soufiane.asx@gmail.com>
2025-05-25 21:58:28 +0200

    maj des scripts Kafka

--- scripts/5d_run_cliD.sh
+++ scripts/5d_run_cliD.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 export application_monnom=ClientD
-cd ../shellClient/target
+
+
+#  dossier du client
+cd "$(dirname "$0")/../shellClient"
+
+#  Compilation automatique si le JAR n'existe pas
+if [ ! -f target/shellClient-0.0.1.jar ]; then
+    echo " Compilation du client (shellClient)..."
+    ./mvnw clean package -DskipTests
+fi
+
+#  Lancement du client
+cd target
 java -jar shellClient-0.0.1.jar
